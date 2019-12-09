aa3d8a9e08097fcf43aa80eedd5d216732215195
Sharon <55546221+syslam@users.noreply.github.com>
2019-12-08 23:26:43 -0500

    Increased size of training and test data by 5x
    Compromise between the full data set (10x the size) and the original limited set. This gets predictor accuracy to above 90% for all classes (digits). Full set gets about half the classes to 100% and half to above 95% but takes far more time and computing power.

--- docs/script.js
+++ docs/script.js
@@ -119,8 +119,8 @@ async function train(model, data) {
   const fitCallbacks = tfvis.show.fitCallbacks(container, metrics);
   
   const BATCH_SIZE = 512;
-  const TRAIN_DATA_SIZE = 5500;
-  const TEST_DATA_SIZE = 1000;
+  const TRAIN_DATA_SIZE = 27500;
+  const TEST_DATA_SIZE = 5000;
 
   const [trainXs, trainYs] = tf.tidy(() => {
     const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
